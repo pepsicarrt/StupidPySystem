39cbb9f7885a9332c16910674f41e248dbb82ac0
macchiato <noun44963@gmail.com>
2025-05-11 12:00:31 -0400

    Added 3 more commands and 2 comments.

--- StupidPySystem.py
+++ StupidPySystem.py
@@ -8,6 +8,7 @@ somethingrandom = ["Pycharm", "Zen", "Firefox", "Rufus", "qBittorrent", "Steam",
 quit = False
 while not quit:
     user = input("> ").lower().strip()
+# This is the Help command.
     if user == "help":
         print("could you like specify what you need help with")
         user = input("help> ").lower().strip()
@@ -20,17 +21,29 @@ while not quit:
             print("Powershell\n// Opens Powershell\n// Quits SPS")
         elif user == "diskmgmt":
             print("Disk Management\n// Opens Disk Management\nAA")
+        elif user == "spsver":
+            print("SPS Ver\n// Just tells you the version of SPS")
+        elif user == ("winver"):
+            print("winver\n// Opens winver")
+        elif user == ("msinfo32"):
+            print("msinfo32\n// Opens msinfo32")
         elif user == "quit":
             print("Quit\n// Quits SPS")
         else:
             print("Command doesn't exist.")
-
+# All of the Commands
     elif user == "program":
         print(random.choice(somethingrandom))
     elif user == "cmd":
         print("opening cmd")
         os.system("cmd")
         quit = True
+    elif user == "spsver":
+        print("StupidPySystem\n// Version 1B\n// Windows Build\n// made by @pepsicart on Discord\n// pepsicarrt on GitHub")
+    elif user == "winver":
+        os.system("winver")
+    elif user == "msinfo32":
+        os.system("msinfo32")
     elif user == "powershell":
         print("opening powershell\nmight take some time")
         os.system("powershell")
